939732a7869d6d9a0352624fa1d7b6de975fdbf8
Peter Gazdík <xgazdi03@stud.fit.vutbr.cz>
2016-04-21 18:15:23 +0200

    Make awesome script for running experiments

--- bin/run.sh
+++ bin/run.sh
@@ -1,29 +1,66 @@
 #!/bin/bash
 
-del="_"
-suffix=".txt"
-
-thresholds="5 6 7 8 9 10 11 12 13 14 15 20 25"
-models="classic layered"
-
-
-for thr in $thresholds; do
-  for mod in $models; do
-    file=$mod$del$thr$suffix
-    ./clMarkovGen -s stats/rockyou.wstat -d dictionaries/phpbb_1-7.dic -t $thresholds -l 1:7 -g 20480000 --model $mod > "results/$file"
-  done
-done
-
-thresholds="5 6 7 8 9 10 15 20"
-limits="50 64 30"
-
-for thr in $thresholds; do
-  for mod in $models; do
-    for lim in $limits; do
-      file=$mod$del$thr$del$lim$suffix
-      ./clMarkovGen -s stats/rockyou.wstat -d dictionaries/phpbb_1-7.dic -t $thresholds:$lim -l 1:7 -g 20480000 --model $mod > "results/$file"
+# stat dict thresholds min max
+function run_simple 
+{ 
+  stat=$1
+  dict=$2
+  thresholds=$3
+  min=$4
+  max=$5
+  models="classic layered"
+  
+  for thr in $thresholds; do
+    for mod in $models; do   
+      out_file="$mod-$thr.txt"    
+      echo "$mod $thr"
+      ./clMarkovGen -s "stats/$stat.wstat" -d "dictionaries/$dict.dic" -t $thr -l "$min:$max" -g 10240000 --model $mod > "results/$stat-$dict/$out_file"
     done
   done
-done
+}
+
+# stat dict thresholds limits min max
+function run_comp {
+  stat=$1
+  dict=$2
+  thresholds=$3
+  limits=$4
+  min=$5
+  max=$6 
+  models="classic layered"
+  
+  for thr in $thresholds; do
+    for mod in $models; do   
+      for lim in $limits; do
+        out_file="$mod-$thr-$lim.txt"
+        echo "$mod $thr:$lim"   
+        ./clMarkovGen -s "stats/$stat.wstat" -d "dictionaries/$dict.dic" -t "$thr:$lim" -l "$min:$max" -g 10240000 --model $mod > "results/$stat-$dict/$out_file"
+      done
+    done
+  done
+}
+
+stat=$1
+dict=$2
+min=$3
+max=$4 
+
+mkdir "results/$stat-$dict"
+
+thresholds="5 6 7 8 9 10 11 12 13 14 15 20 25 30 35 40 45 50 55 60 65 70 75"
+#run_simple $stat $dict "$thresholds" $min $max
+
+thresholds="5 6 7 8 9 10 11 12 13 14 15 20 25 30 35 40 45 50"
+limits="50"
+#run_comp $stat $dict "$thresholds" "$limits" $min $max
+
+thresholds="5 6 7 8 9 10 11 12 13 14 15 20 25 30 35 40 45 50 55 60 64"
+limits="64"
+#run_comp $stat $dict "$thresholds" "$limits" $min $max
+
+thresholds="5 6 7 8 9 10 11 12 13 14 15 20 25 30"
+limits="30"
+run_comp $stat $dict "$thresholds" "$limits" $min $max
+
 
 
